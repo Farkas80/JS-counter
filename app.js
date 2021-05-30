dd94d26a117f0702676759da341daa5035f65a8e
Farkas80 <csaba.farkas80@gmail.com>
2021-05-30 20:48:52 +0200

    Add js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let mainNum = document.getElementById('mainNum');
 let moreBtn = document.getElementsByClassName('more');
-let lessBtn = document.getElementsByClassName('less'),
+let lessBtn = document.getElementsByClassName('less');
 
 moreBtn.addEventListener('click', (e) => {
     e.preventDefault();
